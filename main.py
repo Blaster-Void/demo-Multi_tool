cb7bcb9db7e2ac91e0b165c75d276bce851f73ae
BVoid <atlas.pywork@gmail.com>
2026-09-07 08:55:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -23,28 +23,40 @@ while True:
         if User_input.lower()=='l':
             print(f'{hostname} local ip:{colors['yelow']}{socket.gethostbyname(hostname)}{colors['defalt']}')
 
-        elif User_input.lower=='p':
-
+        elif User_input.lower()=='p':
+            print('a')
             print(f'{colors['grey']}Internet most connected... Please wait a moment.{colors['defalt']}',end='\r',flush=True)
             time.sleep(0.3)
             ip = get('https://api.ipify.org').content.decode('utf8')
             print(f'{hostname} public ip:{colors['yelow']}{ip}                         {colors['defalt']}')
 
     elif User_input.lower()=='c':
+
         date=datetime.datetime.now()
         year=date.year
         month=date.month
         day=date.day
+        weekday=datetime.datetime(year=year,month=month,day=day).strftime('%A')
         rangeofm=calendar.monthrange(year=year,month=month)
         rangeofm=rangeofm[-1]
         monthname=date.strftime('%b')
-        print(monthname)
+
+        
+
+
+        print(f'{colors["yelow"]}{monthname}{colors["defalt"]}')
+        print(f'{weekday}')
+
         days=[i for i in range(1,rangeofm+1)]
         for i in days:
             if day==i:
-                print(f"{colors['red']}{i:2} {colors['defalt']}",end='')
+                if i%7==0:
+                    print(f"{colors['red']}{i:2} {colors['defalt']}",end='\n')
+                    continue
+                else:
+                    print(f"{colors['red']}{i:2} {colors['defalt']}",end='')
+                    continue
                 
-                continue
             if i%7==0:
                 print(f'{i:2}')
             else:
